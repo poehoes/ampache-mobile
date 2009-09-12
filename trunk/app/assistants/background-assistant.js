fece974c0955ce1a9632cc35aaffc25e2c4bf609
bjgeiser <bjgeiser@b86e75ea-92ce-11de-a3eb-1f1a3573c213>
2009-09-12 07:53:39 +0000

    * More background stuff

--- trunk/app/assistants/background-assistant.js
+++ trunk/app/assistants/background-assistant.js
@@ -2,14 +2,14 @@
 BackgroundAssistant = Class.create({
     CurrentColor: null,
     CurrentImage: null,
-	CurrentMode:null,
+    CurrentMode: null,
     
     
     setup: function(){
     
     
-       
-        
+    
+    
         this.wallpaperTypeModel = {
             value: AmpacheMobile.settingsManager.settings.BackgroundMode,
             disabled: false
@@ -28,12 +28,53 @@ BackgroundAssistant = Class.create({
         this.controller.get('wallpaperType').observe(Mojo.Event.propertyChange, this.wallpaperTypeChanged.bind(this));
         
         this.controller.setupWidget("btnChooseWallpaperImage", this.attributes = {}, this.model = {
-            label: "Choose Wallpaper Image",
+            label: "Image From Device",
             disabled: false
         });
         
         this.controller.get('btnChooseWallpaperImage').observe(Mojo.Event.tap, this.handleChooseWallpaperImage.bind(this));
         
+        //***************************************************************
+        // Included Photo picker
+        
+        var photoAttributes = {            //noExtractFS : true	//optional, turn off using extractfs to speed up renders.
+        };
+        this.photoModel = {
+            //backgroundImage : 'images/glacier.png',
+            //background: 'black',		//You can set an image or a color
+            onLeftFunction: this.wentLeft.bind(this),
+            onRightFunction: this.wentRight.bind(this)
+        }
+        this.controller.setupWidget('myPhotoDiv', photoAttributes, this.photoModel);
+        this.myPhotoDivElement = $('myPhotoDiv');
+        
+        this.imageViewChanged = this.imageViewChanged.bindAsEventListener(this);
+        Mojo.Event.listen(this.controller.get('myPhotoDiv'), Mojo.Event.imageViewChanged, this.imageViewChanged);
+        
+        this.solidImages = [];
+        this.solidImages[0] = {
+            source: 'images/backgrounds/solids/background1.png',
+        }
+        this.solidImages[1] = {
+            source: 'images/backgrounds/solids/background2.png',
+        }
+        this.solidImages[2] = {
+            source: 'images/backgrounds/solids/background3.png',
+        }
+		  this.solidImages[3] = {
+            source: 'images/backgrounds/solids/background4.png',
+        }
+		 this.solidImages[4] = {
+           source: 'images/backgrounds/solids/background5.png',
+        }
+		 this.solidImages[5] = {
+            source: 'images/backgrounds/solids/background6.png',
+        }
+		
+		
+		
+		
+        this.solidIndex = 0;
         
         //***************************************************************
         // Overlay Selector
@@ -60,11 +101,11 @@ BackgroundAssistant = Class.create({
             label: "Overlay 4",
             value: "images/backgrounds/overlay/overlay4.png"
         };
-         this.Overlays[5] = {
+        this.Overlays[5] = {
             label: "Overlay 5",
             value: "images/backgrounds/overlay/overlay5.png"
         };
-		
+        
         
         this.selectorAttributes = {
             label: 'Overlay',
@@ -87,49 +128,107 @@ BackgroundAssistant = Class.create({
         this.controller.get('body_wallpaper').style.backgroundColor = AmpacheMobile.settingsManager.settings.BackgroundColor;
         
         this.CurrentMode = AmpacheMobile.settingsManager.settings.BackgroundMode;
-		this.CurrentColor = AmpacheMobile.settingsManager.settings.BackgroundColor;
+        this.CurrentColor = AmpacheMobile.settingsManager.settings.BackgroundColor;
         this.CurrentImage = AmpacheMobile.settingsManager.settings.BackgroundImage;
         
         this.ColorSelector = new ColorPicker(this.controller, this.CurrentColor, this.ColorChangedCallback.bind(this));
         this.ColorSelector.makeColorSelectors();
-		
-		
+        
+        
         this.wallpaperTypeChanged();
     },
     
+    imageViewChanged: function(event){
+        /* Do something when the image view changes */
+        //this.showDialogBox("Image View Changed", "Flick image left and/or right to see other images.");
+    
+		//this.CurrentMode = 1;
+        //this.CurrentImage = event.url;
+        //this.UpdateScreen();
+	},
+    
+    
+    // This function will popup a dialog, displaying the message passed in.
+    showDialogBox: function(title, message){
+        this.controller.showAlertDialog({
+            onChoose: function(value){
+            },
+            title: title,
+            message: message,
+            choices: [{
+                label: 'OK',
+                value: 'OK',
+                type: 'color'
+            }]
+        });
+    },
+    
+    wentLeft: function(event){
+        this.solidIndex = (this.solidIndex - 1) % this.solidImages.length;
+        if (this.solidIndex == -1) 
+            this.solidIndex = this.solidImages.length - 1;
+        this.redoSolidImages(this.solidIndex);
+		
+		this.CurrentMode = 1;
+        this.CurrentImage = this.solidImages[this.solidIndex].source;
+        this.UpdateScreen();
+    },
+    
+    wentRight: function(event){
+        /* Do something when the user flicks to the left 
+         * like picking a different image for the right image.
+         */
+        this.solidIndex = (this.solidIndex + 1) % this.solidImages.length;
+        this.redoSolidImages(this.solidIndex);
+        
+		this.CurrentMode = 1;
+        this.CurrentImage = this.solidImages[this.solidIndex].source;
+        this.UpdateScreen();
+		
+        //this.showDialogBox("Image View Changed", "Flicked left to see right picture.");
+    },
+    
+    
+    redoSolidImages: function(centerIndex){
+        var leftIndex = (centerIndex - 1) % this.solidImages.length;
+        if (leftIndex == -1) 
+            leftIndex = this.solidImages.length - 1;
+        var rightIndex = (centerIndex + 1) % this.solidImages.length;
+        
+        this.myPhotoDivElement.mojo.leftUrlProvided(this.solidImages[leftIndex].source, this.solidImages[leftIndex].source);
+        this.myPhotoDivElement.mojo.centerUrlProvided(this.solidImages[centerIndex].source, this.solidImages[centerIndex].source);
+        this.myPhotoDivElement.mojo.rightUrlProvided(this.solidImages[rightIndex].source, this.solidImages[rightIndex].source);
+    },
     
     
     ColorChangedCallback: function(hexColor){
-		this.CurrentMode = 0;
+        this.CurrentMode = 0;
         this.CurrentColor = hexColor;
-		this.UpdateScreen();
+        this.UpdateScreen();
         
     },
     
-	
-	overlaySelectorChanged: function(event){
+    
+    overlaySelectorChanged: function(event){
         this.CurrentMode = 0;
-		this.CurrentImage = event.value;
-        this.UpdateScreen();     
+        this.CurrentImage = event.value;
+        this.UpdateScreen();
     },
     
-	UpdateScreen:function()
-	{
-		this.controller.get('body_wallpaper').style.background = "url('" + this.CurrentImage + "')";
+    UpdateScreen: function(){
+        this.controller.get('body_wallpaper').style.background = "url('" + this.CurrentImage + "')";
         this.controller.get('body_wallpaper').style.backgroundColor = this.CurrentColor;
-	},
-	
+    },
+    
     
     
     
     
     handleChooseWallpaperImage: function(){
-        Mojo.FilePicker.pickFile(
-		{
+        Mojo.FilePicker.pickFile({
             onSelect: this.photoSelected.bind(this),
             kinds: ['image']
-        }, 
-		this.controller.stageController);
+        }, this.controller.stageController);
         
     },
     
@@ -138,99 +237,96 @@ BackgroundAssistant = Class.create({
         
         // Color
         if (this.wallpaperTypeModel.value == 0) {
-		
-			this.controller.get('image-container').style.display = 'none';
-			this.controller.get('color-container').style.display = 'block';
-		}
-		
-		// Image
-		else {
-			if (this.wallpaperTypeModel.value == 1) {
-			
-			
-			
-				this.controller.get('color-container').style.display = 'none';
-				this.controller.get('image-container').style.display = 'block';
-			}
-		}
-		this.UpdateScreen();      
+        
+            this.controller.get('image-container').style.display = 'none';
+            this.controller.get('color-container').style.display = 'block';
+        }
+        
+        // Image
+        else {
+            if (this.wallpaperTypeModel.value == 1) {
+                
+                
+                this.controller.get('color-container').style.display = 'none';
+                this.controller.get('image-container').style.display = 'block';
+            }
+        }
+        this.UpdateScreen();
     },
     
-   
+    
     
     photoSelected: function(results){
-        
-		this.CurrentMode = 1;
-		this.CurrentImage = results.fullPath;
+    
+        this.CurrentMode = 1;
+        this.CurrentImage = results.fullPath;
         this.UpdateScreen();
-		
-		
-		
+        
+        
+        
     },
     
     
     
     activate: function(){
+    	this.redoSolidImages(this.solidIndex);
+                this.myPhotoDivElement.mojo.manualSize('320', '200');
     
-	
-	},
-	
-	saveSettings:function(value)
-	{
-		if(value=="yes")
-		{
-			AmpacheMobile.settingsManager.settings.BackgroundColor = this.CurrentColor;
-        	AmpacheMobile.settingsManager.settings.BackgroundImage = this.CurrentImage;
-        	AmpacheMobile.settingsManager.settings.BackgroundMode = this.CurrentMode;
-        	AmpacheMobile.settingsManager.SaveSettings();
-		}
-		this.controller.stageController.popScene();
-	},
-	
-	
-	handleCommand: function(event){
-		//test for Mojo.Event.back, not Mojo.Event.command..
-		if (event.type == Mojo.Event.back) {
-			
-			if (this.anyChanges()) {
-				event.preventDefault();
-				event.stopPropagation();
-				
-				this.controller.showAlertDialog({
-					onChoose: this.saveSettings.bind(this),
-					title: $L("Save Changes"),
-					message: "Background settings have changed, would you like to save the new settings?",
-					choices: [{
-						label: $L('Yes'),
-						value: 'yes',
-						type: 'affirmative'
-					},
-					{
-						label: $L('No'),
-						value: 'no',
-						type: 'negative'
-					}]
-				});
-				
-			}
-		}
-	},
-	
-  	anyChanges:function()
-	{
-		var retVal = false;
-		if(AmpacheMobile.settingsManager.settings.BackgroundColor != this.CurrentColor || AmpacheMobile.settingsManager.settings.BackgroundImage != this.CurrentImage)
-		{
-			retVal = true;	
-		}
-		return retVal;
-	},
-	
+    },
     
-	
+    saveSettings: function(value){
+        if (value == "yes") {
+            AmpacheMobile.settingsManager.settings.BackgroundColor = this.CurrentColor;
+            AmpacheMobile.settingsManager.settings.BackgroundImage = this.CurrentImage;
+            AmpacheMobile.settingsManager.settings.BackgroundMode = this.CurrentMode;
+            AmpacheMobile.settingsManager.SaveSettings();
+        }
+        this.controller.stageController.popScene();
+    },
+    
+    
+    handleCommand: function(event){
+        //test for Mojo.Event.back, not Mojo.Event.command..
+        if (event.type == Mojo.Event.back) {
+        
+            if (this.anyChanges()) {
+                event.preventDefault();
+                event.stopPropagation();
+                
+                this.controller.showAlertDialog({
+                    onChoose: this.saveSettings.bind(this),
+                    title: $L("Save Changes"),
+                    message: "Background settings have changed, would you like to save the new settings?",
+                    choices: [{
+                        label: $L('Yes'),
+                        value: 'yes',
+                        type: 'affirmative'
+                    }, {
+                        label: $L('No'),
+                        value: 'no',
+                        type: 'negative'
+                    }]
+                });
+                
+            }
+        }
+    },
+    
+    anyChanges: function(){
+        var retVal = false;
+        if (AmpacheMobile.settingsManager.settings.BackgroundColor != this.CurrentColor || AmpacheMobile.settingsManager.settings.BackgroundImage != this.CurrentImage) {
+            retVal = true;
+        }
+        return retVal;
+    },
+    
+    
+    
     deactivate: function(){
         this.controller.get('body_wallpaper').style.background = null;
         this.controller.get('body_wallpaper').style.backgroundColor = "white";
+		
+		Mojo.Event.stopListening(this.controller.get('myPhotoDiv'),Mojo.Event.imageViewChanged,this.imageViewChanged);
         
     }
     
